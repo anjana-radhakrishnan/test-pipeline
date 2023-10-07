1e7b1672a3ab3de08dd090942f2f8abe1ebf3c06
anjana-radhakrishnan <111497171+anjana-radhakrishnan@users.noreply.github.com>
2023-10-07 22:17:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
+LABEL=$LABEL
 
 REG_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${ORGANIZATION}/test-pipeline/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION}/test-pipeline  --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION}/test-pipeline  --token ${REG_TOKEN} --labels ${LABEL}
 
 cleanup() {
     echo "Removing runner..."
